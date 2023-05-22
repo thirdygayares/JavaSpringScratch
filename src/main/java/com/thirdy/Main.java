ff37e7929c2f3c8ca368531dae98282edcc4903d
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-05-22 18:51:33 +0800

    elaborate the api

--- src/main/java/com/thirdy/Main.java
+++ src/main/java/com/thirdy/Main.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -18,5 +20,37 @@ public class Main {
         return new GreetResponse("Hello");
     }
 
-    record GreetResponse(String greet){}
+    //record GreetResponse(String greet){}
+    class GreetResponse {
+        private final String greet;
+
+        public GreetResponse(String greet) {
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public String toString() {
+            return "GreetResponse{" +
+                    "greet='" + greet + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GreetResponse that = (GreetResponse) o;
+            return Objects.equals(greet, that.greet);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(greet);
+        }
+    }
+
 }
