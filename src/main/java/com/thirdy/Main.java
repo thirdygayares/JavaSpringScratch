d5cb6d72c9b6d1799735d17b4a18df0cf6abef10
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-05-22 18:41:34 +0800

    api in JSON format

--- src/main/java/com/thirdy/Main.java
+++ src/main/java/com/thirdy/Main.java
@@ -14,7 +14,9 @@ public class Main {
     }
 
     @GetMapping("/greet")
-    public String greet(){
-        return "Hello";
+    public GreetResponse greet(){
+        return new GreetResponse("Hello");
     }
+
+    record GreetResponse(String greet){}
 }
